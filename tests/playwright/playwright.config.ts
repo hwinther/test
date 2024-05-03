fbe06658adb0c17b17a5586ed3251e7e365e4e05
Hans Christian Winther-Sorensen <hc@wsh.no>
2024-05-03 19:52:07 +0200

    Add dotenv

--- tests/playwright/playwright.config.ts
+++ tests/playwright/playwright.config.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { defineConfig, devices } from '@playwright/test';
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+import { config } from 'dotenv';
+config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
