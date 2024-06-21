25d2136328537d37d6460138bf02aa0fe07b2b8b
Hans Christian Winther-Sorensen <hc@wsh.no>
2024-06-21 20:09:39 +0200

    TEST-1234 Add validator

---
+++ src/backend/WebApi/Entities/BlogPostDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Entities;
+
+/// <summary>
+///     TODO
+/// </summary>
+public class BlogPostDto : IValidatableObject
+{
+    /// <summary>
+    ///     TODO
+    /// </summary>
+    public string? Test { get; set; }
+
+    /// <inheritdoc />
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (string.IsNullOrEmpty(Test))
+            yield return new ValidationResult($"{nameof(Test)} must be set");
+    }
+}
---
+++ src/backend/WebApi/Filters/ValidateModelAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Filters;
+
+/// <summary>
+///     TODO
+/// </summary>
+public class ValidateModelAttribute : ActionFilterAttribute
+{
+    /// <inheritdoc />
+    public override void OnActionExecuting(ActionExecutingContext actionExecutingContext)
+    {
+        if (!actionExecutingContext.ModelState.IsValid)
+            actionExecutingContext.Result = new BadRequestObjectResult(actionExecutingContext.ModelState);
+    }
+}
