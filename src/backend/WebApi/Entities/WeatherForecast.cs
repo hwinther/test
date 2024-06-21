392ca80bedb06c24193f314e096cf20e38c9648d
Hans Christian Winther-Sorensen <hc@wsh.no>
2024-06-21 23:47:12 +0200

    TEST-1000 Add examples

--- src/backend/WebApi/Entities/WeatherForecast.cs
+++ src/backend/WebApi/Entities/WeatherForecast.cs
@@ -8,15 +8,18 @@ public class WeatherForecast
     /// <summary>
     ///     Date of forecast
     /// </summary>
+    /// <example>2024.06.07</example>
     public DateOnly Date { get; set; }
 
     /// <summary>
     ///     Temperature in celsius
     /// </summary>
+    /// <example>25</example>
     public int TemperatureC { get; set; }
 
     /// <summary>
     ///     Summary text
     /// </summary>
+    /// <example>Sunny</example>
     public string? Summary { get; set; }
 }
