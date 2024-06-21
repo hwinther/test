8750437c0a078458c5894c8673072565e45dd2bb
Hans Christian Winther-Sorensen <hc@wsh.no>
2024-06-21 23:48:24 +0200

    TEST-1000 Update api services and models

--- src/frontend/src/api/endpoints/weather-forecast/weather-forecast.ts
+++ src/frontend/src/api/endpoints/weather-forecast/weather-forecast.ts
@@ -1,22 +1,15 @@
-import type { QueryFunction, QueryKey, UseQueryOptions, UseQueryResult } from '@tanstack/react-query'
-
+//@ts-nocheck
 import { useQuery } from '@tanstack/react-query'
-
+import type { QueryFunction, QueryKey, UseQueryOptions, UseQueryResult } from '@tanstack/react-query'
 import type { WeatherForecast } from '../../models'
+import { customInstance } from '../../mutators/custom-instance'
 import type { ErrorType } from '../../mutators/custom-instance'
 
-import { customInstance } from '../../mutators/custom-instance'
-
 /**
- * @param signal
  * @summary Returns weather forecast
  */
-export const getWeatherForecast = async (signal?: AbortSignal) => {
-  return await customInstance<WeatherForecast[]>({
-    method: 'GET',
-    signal,
-    url: `/WeatherForecast`,
-  })
+export const getWeatherForecast = (signal?: AbortSignal) => {
+  return customInstance<WeatherForecast[]>({ url: `/WeatherForecast`, method: 'GET', signal })
 }
 
 export const getGetWeatherForecastQueryKey = () => {
@@ -33,22 +26,20 @@ export const getGetWeatherForecastQueryOptions = <
 
   const queryKey = queryOptions?.queryKey ?? getGetWeatherForecastQueryKey()
 
-  const queryFn: QueryFunction<Awaited<ReturnType<typeof getWeatherForecast>>> = async ({ signal }) =>
-    await getWeatherForecast(signal)
+  const queryFn: QueryFunction<Awaited<ReturnType<typeof getWeatherForecast>>> = ({ signal }) =>
+    getWeatherForecast(signal)
 
-  return { queryFn, queryKey, ...queryOptions } as { queryKey: QueryKey } & UseQueryOptions<
+  return { queryKey, queryFn, ...queryOptions } as UseQueryOptions<
     Awaited<ReturnType<typeof getWeatherForecast>>,
     TError,
     TData
-  >
+  > & { queryKey: QueryKey }
 }
 
 export type GetWeatherForecastQueryResult = NonNullable<Awaited<ReturnType<typeof getWeatherForecast>>>
 export type GetWeatherForecastQueryError = ErrorType<unknown>
 
 /**
- * @param options
- * @param options.query
  * @summary Returns weather forecast
  */
 export const useGetWeatherForecast = <
@@ -56,12 +47,10 @@ export const useGetWeatherForecast = <
   TError = ErrorType<unknown>,
 >(options?: {
   query?: Partial<UseQueryOptions<Awaited<ReturnType<typeof getWeatherForecast>>, TError, TData>>
-}): { queryKey: QueryKey } & UseQueryResult<TData, TError> => {
+}): UseQueryResult<TData, TError> & { queryKey: QueryKey } => {
   const queryOptions = getGetWeatherForecastQueryOptions(options)
 
-  const query = useQuery(queryOptions) as {
-    queryKey: QueryKey
-  } & UseQueryResult<TData, TError>
+  const query = useQuery(queryOptions) as UseQueryResult<TData, TError> & { queryKey: QueryKey }
 
   query.queryKey = queryOptions.queryKey
 
