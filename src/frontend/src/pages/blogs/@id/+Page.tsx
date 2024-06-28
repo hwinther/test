7ca89d58790233e42d8dd501240d2b6179004fcf
Hans Christian Winther-Sorensen <hc@wsh.no>
2024-06-28 22:31:39 +0200

    TEST-0004 Add blog post details page

---
+++ src/frontend/src/pages/blogs/@id/+Page.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { usePageContext } from 'vike-react/usePageContext'
+
+import { useGetBlog, useGetPosts } from '~/api/endpoints/blogging/blogging'
+import { Link } from '~/renderer/Link'
+
+const BlogListView: React.FC = () => {
+  const pageContext = usePageContext()
+  const id = pageContext?.routeParams?.id ?? window.location.pathname.split('/').at(-1)
+  const { data: blog, error: blogError, isLoading: blogIsLoading } = useGetBlog(parseInt(id ?? '0'))
+  const { data: posts, error: postsError, isLoading: postsIsLoading } = useGetPosts(parseInt(id ?? '0'))
+
+  if (blogIsLoading || postsIsLoading) return <div>Loading...</div>
+  if (blogError != null || blog === null || postsError != null || posts === null) return <div>Error loading blog</div>
+
+  return (
+    <div>
+      <h2>
+        Blog: {blog?.title} - {blog?.url}
+      </h2>
+      <h3>Posts</h3>
+      <ul>
+        {posts?.map((post) => (
+          <li key={post.postId}>
+            <p>
+              <Link className="is-active" href={`/blog/${blog?.blogId}/post/${post?.postId}`}>
+                {post.title} - ({post.content})
+              </Link>
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default BlogListView
--- src/frontend/src/renderer/Link.tsx
+++ src/frontend/src/renderer/Link.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-//import { usePageContext } from 'vike-react/usePageContext'
+import { usePageContext } from 'vike-react/usePageContext'
 
 export interface LinkProps {
   children: React.ReactNode
@@ -13,12 +13,16 @@ export interface LinkProps {
  * @returns {React.JSX.Element} The rendered component.
  */
 function Link(props: Readonly<LinkProps>): React.JSX.Element {
-  // With SSR:
-  // const pageContext = usePageContext()
-  // const { urlPathname } = pageContext
-  const urlPathname = window.location.pathname
+  const pageContext = usePageContext()
+  let pathname = window.location.pathname
+
+  if (pageContext !== undefined) {
+    const { urlPathname } = pageContext
+    pathname = urlPathname
+  }
+
   const { href } = props
-  const isActive = href === '/' ? urlPathname === href : urlPathname.startsWith(href)
+  const isActive = href === '/' ? pathname === href : pathname.startsWith(href)
   const className = [props.className, isActive && 'is-active'].filter(Boolean).join(' ')
   return (
     <a {...props} className={className}>
