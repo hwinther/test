8edd7c8c47401de76fa6331d6515cc69ac698d55
Hans Christian Winther-Sorensen <hc@wsh.no>
2025-10-27 22:29:56 +0100

    fix: Unspecific types

--- src/frontend/src/renderer/+onRenderHtml.tsx
+++ src/frontend/src/renderer/+onRenderHtml.tsx
@@ -3,17 +3,17 @@ import { escapeInject } from 'vike/server'
 
 /**
  * Renders the HTML template.
- * @returns {Promise<any>} The wrapped template.
+ * @returns {Promise<ReturnType<typeof escapeInject>>} The wrapped template.
  */
-async function onRenderHtml(): Promise<any> {
+async function onRenderHtml(): Promise<ReturnType<typeof escapeInject>> {
   return escapeInject`
 <!doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vite + React + TS</title>
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
   </head>
   <body>
     <div id="root"></div>
