31e4d346fdc65dd542c5414250533556c9e64c7b
Hans Christian Winther-Sorensen <hc@wsh.no>
2026-03-13 23:53:23 +0100

    Fix newline issue

--- src/backend/WebApi/Program.cs
+++ src/backend/WebApi/Program.cs
@@ -101,6 +101,8 @@ builder.Services.AddSwaggerGen(static options =>
 
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+    options.SwaggerGeneratorOptions.XmlCommentEndOfLine = "\n";
 });
 
 var app = builder.Build();
